42da27d976de849d7c7ce853bd40a4aeeb6ecf18
Lorenzo Sevilla <lorenzosevilla.w@gmail.com>
2015-07-08 18:59:46 -0500

    Added JQuery mansonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require jquery.turbolinks
+//= require masonry/jquery.masonry
+//= require masonry/jquery.imagesloaded.min
 //= require turbolinks
 //= require_tree .
